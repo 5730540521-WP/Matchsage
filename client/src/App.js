3855a7d3b3f09570a2dbff4afe31930ee794c4a1
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-11-15 20:36:58 +0700

    create adminLogin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
+import { Redirect } from 'react-router';
 import logo from './logo.svg';
 // import Helmet from 'react-helmet';
 import Header from './components/Header/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Team from './components/Team';
+import AdminLogin from './components/Admin/Login';
 import Service from './components/Service/Service'
 import NotFound from './components/NotFound';
 import './App.css';
@@ -33,8 +35,17 @@ const AdminRoute = ()=>(
 
 const Body = ({userType})=>{
 	const user = localStorage.getItem('user');
+	const admin = localStorage.getItem('admin');
 	return(
 		<Switch>
+			<Route path='/admin/login' render={() => {
+				if (localStorage.admin) {
+					return (<Redirect to='/admin' />)
+				} else {
+					return (<AdminLogin />)
+				}
+			}} />
+			{/* <Route path="/admin" component={Admin}/> */}
 			<Route path="/team" component={Team}/>
 			{ user
 				? <Route path="/" component={Service}/>
--- client/src/actions/AdminActions.js
+++ client/src/actions/AdminActions.js
@@ -2,9 +2,31 @@ import axios from 'axios';
 import {apiUrl} from '../constants/ConfigConstants';
 import {userConstants} from '../constants/UserConstants';
 
-export const AdminACtions = {
-	informComplaint,
-	restrictPrivilege
+const backendUrl = 'http://128.199.113.165:3001' 
+
+let myheaders = ''
+
+function setHeaderWithAccessToken() {
+	myheaders = new Headers({
+		Authorization: 'JWT ' + localStorage.getItem('admin')
+	})
+}
+
+async function login(payload) {
+	try {
+		const raw = await fetch(`${backendUrl}/api/admin-auth`, {
+			method: 'POST',
+			body: JSON.stringify(payload)
+		})
+
+		const res = await raw.json()
+		if (res.error) throw Error(res.error)
+		localStorage.setItem('admin', res.token)
+		window.currentUser = res.user
+		setHeaderWithAccessToken()
+	} catch (error) {
+		alert(error)
+	}
 }
 
 // Use case: 29
@@ -15,4 +37,10 @@ function informComplaint(){
 // Use case: 30
 function restrictPrivilege(){
 
+}
+
+export {
+	informComplaint,
+	restrictPrivilege,
+	login
 }
