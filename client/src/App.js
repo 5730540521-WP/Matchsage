50bc0241bb16359e1ccaa57c6d6116e56d8b35a8
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-11-16 01:46:44 +0700

    add history

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
-import { Redirect } from 'react-router';
+import { Redirect, Router } from 'react-router';
+import browserHistory from './helpers/history'
 import logo from './logo.svg';
 // import Helmet from 'react-helmet';
 import Header from './components/Header/Header';
@@ -38,30 +39,38 @@ const Body = ({userType})=>{
 	const user = localStorage.getItem('user');
 	const admin = localStorage.getItem('admin');
 	return(
-		<Switch>
-			<Route path='/admin/login' render={() => {
-				if (localStorage.admin) {
-					return (<Redirect to='/admin' />)
-				} else {
-					return (<AdminLogin />)
+		<Router history={browserHistory}>
+			<Switch>
+				<Route path='/admin/login' render={() => {
+					if (localStorage.admin) {
+						return (<Redirect to='/admin' />)
+					} else {
+						return (<AdminLogin />)
+					}
+				}} />
+				{/* <Route path="/admin" component={Admin} render={() => {
+					if (!localStorage.admin) {
+						return (<Redirect to='/admin/login' />)
+					} else {
+						console.log('yey')
+					}
+				}} /> */}
+				<Route path="/admin/" render={() => {
+					if (!localStorage.admin) {
+						return (<Redirect to='/admin/login'/>)
+					} else {
+						return (<AdminSearch />)
+					}
+				}}/>
+				<Route path="/team" component={Team}/>
+				{ user
+					? <Route path="/" component={Service}/>
+					: <Route path="/" component={Home}/>
 				}
-			}} />
-			{/* <Route path="/admin" component={Admin} render={() => {
-				if (!localStorage.admin) {
-					return (<Redirect to='/admin/login' />)
-				} else {
-					console.log('yey')
-				}
-			}} /> */}
-			<Route path="/admin/search" component={AdminSearch}/>
-			<Route path="/team" component={Team}/>
-			{ user
-				? <Route path="/" component={Service}/>
-				: <Route path="/" component={Home}/>
-			}
-			{/* <Route path="/posts/:id" component={}/> */}
-			<Route component={NotFound}/>
-		</Switch>
+				{/* <Route path="/posts/:id" component={}/> */}
+				<Route component={NotFound}/>
+			</Switch>
+		</Router>
 	);
 }
 
