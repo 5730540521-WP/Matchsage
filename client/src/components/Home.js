7a7e0a8b3a4d052e562aceb8e9e232deba5a76a1
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-11-23 01:07:15 +0700

    add steps

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Row, Col,Button } from 'antd';
+import { Row, Col,Button, Icon, Steps } from 'antd';
 import SignupModal from './Common/Modal/SignupModal';
+const Step = Steps.Step;
 
 
 /*const Hero = styled.div.attrs({
@@ -24,11 +25,6 @@ export default class Home extends React.Component{
 	}
 	
 	render(){
-		const text = {
-			textIndent:'50px',
-			textAlign: 'left',
-			fontSize:'30px'
-		}
 		const imgurl1 = './images/1150.jpg',imgurl2 = './images/1142.jpg';
 		return (
 			<section className="section" style={{padding:'0px',backgroundColor:'#FFF8EB'}}>
@@ -37,7 +33,7 @@ export default class Home extends React.Component{
 					<Col span={24}>
 							<img src={imgurl1} style={{display: 'block',margin:'auto',position:'relative',width:'100%',height:'40.1vw',opacity:.9}}/>
 							<div style={{position:'absolute', bottom:'0%', width:'100%', height:'40%', 'backgroundColor':'black', opacity:.4}}></div>
-							<div style={{position:'absolute', bottom:'8%' ,left:'2%',width:'70%',fontSize:'20px',textAlign: 'left'}}>
+							<div style={{position:'absolute', bottom:'8%' ,left:'4%',width:'70%',fontSize:'20px',textAlign: 'left'}}>
 								<h1 style={{fontWeight:'bold', color:'white'}}>
 									Matchsage
 								</h1>					
@@ -49,7 +45,7 @@ export default class Home extends React.Component{
 					</Col>
 					
 				</Row>
-				<div style={{padding:'1.5rem',color:'#402900'}}>
+				<div style={{padding:'4%',color:'#402900'}}>
 				<h1 style={{fontSize:'35px', textAlign: 'left'}}>Matchsage คืออะไร...</h1>
 				<Row gutter={24}>
 					<Col span={12}>
@@ -57,7 +53,12 @@ export default class Home extends React.Component{
 							<img src={imgurl2} style={{borderRadius:'2%',padding:'12px'}}/>
 						</figure>
 					</Col>
-					<Col span={12} style={text}>
+						<Col span={12} style={{
+							textIndent: '50px',
+							textAlign: 'left',
+							fontSize: '25px',
+							marginTop: 20
+						}}>
 						<p>Matchsage เป็นตัวกลางระหว่างร้านนวดจากทั่วประเทศและลูกค้า โดยจะเป็นระบบที่รวบรวมข้อมูลร้านนวดต่างๆทั่วประเทศมารวมกันไว้เพื่อให้สะดวกต่อการค้นหา
 						</p>
 						<br/>
@@ -65,6 +66,33 @@ export default class Home extends React.Component{
 						</p>
 					</Col>
 				</Row>
+				
+				</div>
+				<div style={{backgroundColor: 'white', padding: '4%'}}>
+					<h1 style={{textAlign: 'left', fontSize:35}}>จองบริการสุดง่าย</h1>
+					<Row style={{padding: 50}}>
+						<Col span={3}>
+							<Icon type="user" style={{ fontSize: 90, color: 'orange' }} />
+						</Col>
+						<Col span={3}>
+							<Icon type="arrow-right" style={{ fontSize: 75, color: '#08c' }} />
+						</Col>
+						<Col span={3}>
+							<Icon type="search" style={{ fontSize: 90, color: 'orange' }} />
+						</Col>
+						<Col span={5}>
+							<Icon type="arrow-right" style={{ fontSize: 75, color: '#08c' }} />
+						</Col>
+						<Col span={3}>
+							<Icon type="book" style={{ fontSize: 90, color: 'orange' }} />
+						</Col>
+						<Col span={3}>
+							<Icon type="arrow-right" style={{ fontSize: 75, color: '#08c' }} />
+						</Col>
+						<Col span={3}>
+							<Icon type="credit-card" style={{ fontSize: 90, color: 'orange' }} />
+						</Col>
+					</Row>
 				</div>
 				{/* <div className="">
 					<h1> Welcome to Matchsage </h1>
