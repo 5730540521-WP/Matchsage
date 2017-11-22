528f81d5ce6ac414a550a518cdae403d5f98a905
Wasin Panitansirikul <wasin.p1995@gmail.com>
2017-11-22 18:59:01 +0700

    Add Receipt Model

---
+++ server/src/models/receipt.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const promise = require('bluebird')
+
+const ReceiptSchema = new mongoose.Schema({
+    reservation_id: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    payment_date: {
+        type: String,
+        required: true
+    }
+})
+
+ReceiptSchema.static = {
+    
+    findByReservationId: function (reservationId){
+        return Receipt.findOne({ reservation_id: reservationId })
+    }
+
+    createReceipt: async function (values) {
+        const receiptCount = await Receipt.find().count()
+        values.receipt_id = 'match-rec-' + (receiptCount + 1).toString()
+        return Receipt.create(values)
+    }
+}
+
+const Receipt = mongoose.model('receipt', ReceiptSchema)
+promise.promisifyAll(Receipt)
+
+module.exports = Receipt
