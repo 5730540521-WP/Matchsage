ac23c89345704503b248b83217c4f4007907e3c2
5730447921-PW <bas_kung02@hotmail.com>
2017-11-28 05:22:00 +0700

    fix bug & prevent bug

--- client/src/components/Service/Reservation/DateTimeSelection.js
+++ client/src/components/Service/Reservation/DateTimeSelection.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 
 import {CustomerActions} from 'actions/CustomerActions';
 
-import {LocaleProvider, DatePicker, TimePicker} from 'antd';
+import {Calendar,Row,Col,LocaleProvider, DatePicker, TimePicker} from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 import moment from 'moment';
 
@@ -12,7 +12,7 @@ class DateTimeSelection extends React.PureComponent{
 	constructor(props){
 		super(props);
 		this.state = {
-			date:'',
+			date:moment('2017/11/28', 'YYYY/MM/DD'),
 			start_time:'',
 			timeOptions:{
 				timeZone: "Asia/Bangkok"
@@ -23,6 +23,7 @@ class DateTimeSelection extends React.PureComponent{
 	onSelectDate = ({_d})=>{
 		console.log(_d);
 		const date = _d.toLocaleDateString('en-GB', this.state.timeOptions).split('/').reverse().join('-');//.replace(/\//g,'-');
+		this.setState({date:moment(_d,'YYYY/MM/DD')})
 		this.props.selectDateTimeReservation(date);
 	}
 
@@ -49,15 +50,28 @@ class DateTimeSelection extends React.PureComponent{
 		const timeFormat = 'HH:mm';
 		return(
 			<LocaleProvider locale={enUS}>
-				<div>
-				เลือกวันที่ต้องการ
-				<DatePicker onChange={this.onSelectDate} defaultValue={moment('2017/11/28', dateFormat)} format={dateFormat} />
-				<br/>
-				เลือกเวลาที่ต้องการ
-				<h3>เวลารับบริการ</h3>
-				<TimePicker onChange={this.onSelectStartTime} defaultValue={moment('00:00', timeFormat)} format={timeFormat}/>,			
-				<h3>เวลาสิ้นสุดบริการ</h3><TimePicker onChange={this.onSelectEndTime} defaultValue={moment('00:00', timeFormat)} format={timeFormat}/>,			
-				</div>
+				<Row>
+					<Col span={12}>
+						<h1>เลือกวันที่ต้องการ</h1>
+						<div style={{ width: 290, border: '1px solid #d9d9d9', borderRadius: 4,display:'block',margin:'auto' }}>
+							<Calendar fullscreen={false} value={this.state.date} defaultValue={moment('2017/11/28', dateFormat)} onSelect={this.onSelectDate}/>
+						</div>
+						{/* <DatePicker allowClear={false} onChange={this.onSelectDate} defaultValue={moment('2017/11/28', dateFormat)} format={dateFormat} /> */}
+					</Col>
+					<Col span={12}>
+						<h1>เลือกเวลาที่ต้องการ</h1>
+						<Row type="flex" justify="center" style={{marginTop:'16px'}}>
+							<Col span={10}>
+								<h3 style={{fontSize:'18px',marginBottom:'10px'}}>เวลารับบริการ</h3>
+								<TimePicker allowEmpty={false} onChange={this.onSelectStartTime} defaultValue={moment('00:00', timeFormat)} format={timeFormat}/>		
+							</Col>
+							<Col span={10}>
+								<h3 style={{fontSize:'18px',marginBottom:'10px'}}>เวลาสิ้นสุดบริการ</h3>
+								<TimePicker allowEmpty={false} onChange={this.onSelectEndTime} defaultValue={moment('00:00', timeFormat)} format={timeFormat}/>			
+							</Col>
+						</Row>
+					</Col>
+				</Row>
 			</LocaleProvider>
 		);
 	}
